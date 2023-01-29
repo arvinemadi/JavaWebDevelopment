89350c1c9b21d95165848abcfa02a4709cfaaafa
Arvin Emadi <arvin.emadi@gmail.com>
2023-01-29 00:07:44 -0600

    adding microservices completed project

---
+++ microservices/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -0,0 +1,190 @@
+package com.udacity.vehicles.api;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.udacity.vehicles.client.maps.MapsClient;
+import com.udacity.vehicles.client.prices.PriceClient;
+import com.udacity.vehicles.domain.Condition;
+import com.udacity.vehicles.domain.Location;
+import com.udacity.vehicles.domain.car.Car;
+import com.udacity.vehicles.domain.car.Details;
+import com.udacity.vehicles.domain.manufacturer.Manufacturer;
+import com.udacity.vehicles.service.CarService;
+import java.net.URI;
+import java.util.Collections;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.Charset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Implements testing of the CarController class.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+public class CarControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<Car> json;
+
+    @MockBean
+    private CarService carService;
+
+    @MockBean
+    private PriceClient priceClient;
+
+    @MockBean
+    private MapsClient mapsClient;
+
+    //  https://knowledge.udacity.com/questions/310959
+    private MediaType myContentType = new MediaType("application", "hal+json", Charset.forName("UTF-8"));
+
+    /**
+     * Creates pre-requisites for testing, such as an example car.
+     */
+    @Before
+    public void setup() {
+        Car car = getCar();
+        car.setId(1L);
+        given(carService.save(any())).willReturn(car);
+        given(carService.findById(any())).willReturn(car);
+        given(carService.list()).willReturn(Collections.singletonList(car));
+    }
+
+    /**
+     * Tests for successful creation of new car in the system
+     * @throws Exception when car creation fails in the system
+     */
+    @Test
+    public void createCar() throws Exception {
+        Car car = getCar();
+        mvc.perform(
+                post(new URI("/cars"))
+                        .content(json.write(car).getJson())
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isCreated());
+    }
+
+    /**
+     * Tests if the read operation appropriately returns a list of vehicles.
+     * @throws Exception if the read operation of the vehicle list fails
+     */
+    @Test
+    public void listCars() throws Exception {
+        /**
+         * Add a test to check that the `get` method works by calling
+         *   the whole list of vehicles. This should utilize the car from `getCar()`
+         *   below (the vehicle will be the first in the list).
+         */
+        Car car = getCar();
+        mvc.perform(
+                post(new URI("/cars"))
+                        .content(json.write(car).getJson())
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaType.APPLICATION_JSON_UTF8));
+
+        mvc.perform(get("/cars/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(myContentType));
+
+        verify(carService, times(1)).list();
+    }
+
+    /**
+     * Tests the read operation for a single car by ID.
+     * @throws Exception if the read operation for a single car fails
+     */
+    @Test
+    public void findCar() throws Exception {
+        /**
+         * Add a test to check that the `get` method works by calling
+         *   a vehicle by ID. This should utilize the car from `getCar()` below.
+         */
+        Car car = getCar();
+
+        mvc.perform(
+                post(new URI("/cars"))
+                        .content(json.write(car).getJson())
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaType.APPLICATION_JSON_UTF8));
+
+        mvc.perform(
+                        get(new URI("/cars/1")))
+                .andExpect(status().isOk());
+
+    }
+
+    /**
+     * Tests the deletion of a single car by ID.
+     * @throws Exception if the delete operation of a vehicle fails
+     */
+    @Test
+    public void deleteCar() throws Exception {
+        /**
+         *  Add a test to check whether a vehicle is appropriately deleted
+         *   when the `delete` method is called from the Car Controller. This
+         *   should utilize the car from `getCar()` below.
+         */
+        Car car = getCar();
+        mvc.perform(
+                post(new URI("/cars"))
+                        .content(json.write(car).getJson())
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaType.APPLICATION_JSON_UTF8));
+        mvc.perform(
+                        delete(new URI("/cars/1")))
+                .andExpect(status().isNoContent());
+
+    }
+
+    /**
+     * Creates an example Car object for use in testing.
+     * @return an example Car object
+     */
+    private Car getCar() {
+        Car car = new Car();
+        car.setLocation(new Location(20.1, -30.1));
+        Details details = new Details();
+        Manufacturer manufacturer = new Manufacturer(50, "Buick");
+        details.setManufacturer(manufacturer);
+        details.setModel("Encore");
+        details.setMileage(32280);
+        details.setExternalColor("blue");
+        details.setBody("suv");
+        details.setEngine("2.4L V4");
+        details.setFuelType("Gas");
+        details.setModelYear(2023);
+        details.setProductionYear(2022);
+        details.setNumberOfDoors(4);
+        car.setDetails(details);
+        car.setCondition(Condition.NEW);
+        return car;
+    }
+}
